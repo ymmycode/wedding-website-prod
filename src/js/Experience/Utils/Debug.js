c272f40293d345d0d0691d7a21bafad898da58ed
Achmat Fauzi <achmatfauzi86@gmail.com>
2022-04-04 14:42:18 +0700

    Using lilgui to debug

--- src/js/Experience/Utils/Debug.js
+++ src/js/Experience/Utils/Debug.js
@@ -1,9 +1,27 @@
-
+import Experience from "../Experience";
+import GUI from 'three/examples/jsm/libs/lil-gui.module.min'
 
 export default class Debug 
 {
     constructor()
     {
+        // experience
+        this.experience = new Experience()
+
+        // debug gui
+        // this.active = window.location.hash === `#debug` 
+
+        // this.active = true
+        this.active = false
+
+        this.gui = new GUI()
+        this.gui.hide()
+
+        if(this.active)
+        {
+            this.gui.show()
+        }
+
         
     }
 }
