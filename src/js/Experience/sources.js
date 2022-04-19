b6de578bc4f2cb434d5af2e688f56a7af5416d2f
Achmat Fauzi <achmatfauzi86@gmail.com>
2022-04-19 23:29:43 +0700

    changing asset path for spa

--- src/js/Experience/sources.js
+++ src/js/Experience/sources.js
@@ -1,237 +1,229 @@
 export default 
 [
     {
-        // texture for environment (cube hdri)
-        name: 'environmentMapTexture',
-        type: 'cubeTexture',
-        path:
-        [
-            'textures/environmentMap/px.jpg',
-            'textures/environmentMap/nx.jpg',
-            'textures/environmentMap/py.jpg',
-            'textures/environmentMap/ny.jpg',
-            'textures/environmentMap/pz.jpg',
-            'textures/environmentMap/nz.jpg'
-        ]
-    },
-
-    {
         // ring1 alpha texture
         name: 'ring1Alpha',
         type: 'texture',
-        path: 'textures/decal-name/fitri-alpha.jpg'
+        path: '../textures/decal-name/fitri-alpha.jpg'
     },
 
     {
         // ring2 alpha texture
         name: 'ring2Alpha',
         type: 'texture',
-        path: 'textures/decal-name/rizki-alpha.jpg'
+        path: '../textures/decal-name/rizki-alpha.jpg'
     },
 
     {
         // ring1 normal texture
         name: 'ring1Normal',
         type: 'texture',
-        path: 'textures/decal-name/fitri.jpg'
+        path: '../textures/decal-name/fitri.jpg'
     },
 
     {
         // ring2 normal texture
         name: 'ring2Normal',
         type: 'texture',
-        path: 'textures/decal-name/rizki.jpg'
+        path: '../textures/decal-name/rizki.jpg'
     },
 
     {
         // decal name model
         name: 'decalRing1',
         type: 'gltfModel',
-        path: 'models/rings/ring-decal-1.glb'
+        path: '../models/rings/ring-decal-1.glb'
     },
 
     {
         // decal name model
         name: 'decalRing2',
         type: 'gltfModel',
-        path: 'models/rings/ring-decal-2.glb'
+        path: '../models/rings/ring-decal-2.glb'
     },
 
     {
         // base ring model
         name: 'baseRing1',
         type: 'gltfModel',
-        path: 'models/rings/base-ring-1.glb'
+        path: '../models/rings/base-ring-1.glb'
     },
 
     {
         //  base ring model
         name: 'baseRing2',
         type: 'gltfModel',
-        path: 'models/rings/base-ring-2.glb'
+        path: '../models/rings/base-ring-2.glb'
     },
 
     {
         // gate model
         name: 'gateScene',
         type: 'gltfModel',
-        path: 'models/gate/GateScene.glb'
+        path: '../models/gate/GateScene.glb'
     },
 
     {
         // colorPalette
         name: 'flowerPalette',
         type: 'texture',
-        path: 'textures/palette/FlowerPalette.jpg'
+        path: '../textures/palette/FlowerPalette.jpg'
     },
 
     {
         //  colorPalette2
         name: 'mapColorPalette',
         type: 'texture',
-        path: 'textures/palette/ColorPalette.jpg'
+        path: '../textures/palette/ColorPalette.jpg'
     },
 
     {
         // floor shadow
         name: 'floorShadow',
         type: 'texture',
-        path: 'textures/gate-design/ShadowGate1.jpg'
+        path: '../textures/gate-design/ShadowGate1.jpg'
     },
 
     {
         // floor shadow
         name: 'floorShadowAlpha',
         type: 'texture',
-        path: 'textures/gate-design/ShadowGate1Alpha.jpg'
+        path: '../textures/gate-design/ShadowGate1Alpha.jpg'
     },
 
     {
         // photo placeholder
         name: 'photoPlaceholder',
         type: 'texture',
-        path: 'textures/gate-design/Photo.jpg'
+        path: '../textures/gate-design/Photo.jpg'
     },
 
     {
         // wood texture
         name: 'wood',
         type: 'texture',
-        path: 'textures/gate-design/color.jpg'
+        path: '../textures/gate-design/color.jpg'
     },
 
     {
         // flower decoration normal
         name: 'flowerDecoAlpha',
         type: 'texture',
-        path: 'textures/floral-deco/alpha.jpg'
+        path: '../textures/floral-deco/alpha.jpg'
     },
 
     {
         // flower decoration color / alpha
         name: 'flowerDecoColor',
         type: 'texture',
-        path: 'textures/floral-deco/color.jpg'
+        path: '../textures/floral-deco/color.jpg'
     },
 
     {
         // map model
         name: 'mapScene',
         type: 'gltfModel',
-        path: 'models/map/map.glb'
+        path: '../models/map/map.glb'
     },
 
     {
         // map environment baked
         name: 'mapEnv',
         type: 'texture',
-        path: 'textures/map-design/EnvBake.jpg'
+        path: '../textures/map-design/EnvBake.jpg'
     },
 
     {
         // map design
         name: 'mapDesign',
         type: 'texture',
-        path: 'textures/map-design/MapDesign1.png'
+        path: '../textures/map-design/MapDesign1.png'
     },
 
     {
         // partner photo 1
         name: 'partnerPhoto1',
         type: 'texture',
-        path: 'textures/PartnerPhotos/PartnerPhoto1.jpg'
+        path: '../textures/PartnerPhotos/PartnerPhoto1.jpg'
     },
     
     {
         // partner photo 2
         name: 'partnerPhoto2',
         type: 'texture',
-        path: 'textures/PartnerPhotos/PartnerPhoto2.jpg'
+        path: '../textures/PartnerPhotos/PartnerPhoto2.jpg'
     },
     
     {
         // flower decor for photo frame (color)
         name: 'frameDecorColor',
         type: 'texture',
-        path: 'textures/PartnerPhotos/framedecor-color.jpg'
+        path: '../textures/PartnerPhotos/framedecor-color.jpg'
     },
         
     {
         // flower decor for photo frame (alpha)
         name: 'frameDecorAlpha',
         type: 'texture',
-        path: 'textures/PartnerPhotos/framedecor-alpha.jpg'
+        path: '../textures/PartnerPhotos/framedecor-alpha.jpg'
     },
             
     {
         // frame partner photo model
         name: 'photoFrame',
         type: 'gltfModel',
-        path: 'models/frame/PhotoFrame.glb'
+        path: '../models/frame/PhotoFrame.glb'
     },
                             
     {
         // frame partner photo model
         name: 'photoFrame2',
         type: 'gltfModel',
-        path: 'models/frame/PhotoFrame2.glb'
+        path: '../models/frame/PhotoFrame2.glb'
     },
                 
     {
         // Gallery
         name: 'gallery',
         type: 'gltfModel',
-        path: 'models/gallery/gallery.glb'
+        path: '../models/gallery/gallery.glb'
     },
                     
     {
         // gallery texture
         name: 'galleryTexture',
         type: 'texture',
-        path: 'textures/gallery/gallery.jpg'
+        path: '../textures/gallery/gallery.jpg'
     },
     
     {
         // enter button
         name: 'enterButton',
         type: 'texture',
-        path: 'textures/enter/enter-button.jpg'
+        path: '../textures/enter/enter-button.jpg'
     },
 
     {
         // enter button 
         name: 'enterButtonFirst',
         type: 'texture',
-        path: 'textures/enter/enter-button-first.jpg'
+        path: '../textures/enter/enter-button-first.jpg'
     },
 
     {
         // enter button 
         name: 'enterButtonLast',
         type: 'texture',
-        path: 'textures/enter/enter-button-last.jpg'
+        path: '../textures/enter/enter-button-last.jpg'
     },
 
+    {
+        // matcap gold
+        name: 'matcapGold1',
+        type: 'texture',
+        path: '../textures/matcap/matcap-gold-2_darker.png'
+    },
+    
     // {
     //     // 
     //     name: '',
