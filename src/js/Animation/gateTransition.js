a7880675a9937929baced218311d41406c260826
Achmat Fauzi <achmatfauzi86@gmail.com>
2022-04-20 00:20:40 +0700

    changing target animation

--- src/js/Animation/gateTransition.js
+++ src/js/Animation/gateTransition.js
@@ -3,7 +3,7 @@ import gsap from "gsap"
 const gateTransition = (camera) => 
 {
     // text scroll
-    const bgContainer = document.querySelector(`.background-container`)
+    const scroll = document.querySelectorAll(`.scroll`)
     const transition = document.querySelector(`.transition`)
 
     // home leave page / gate entering animation
@@ -17,7 +17,7 @@ const gateTransition = (camera) =>
     tl
     .to(camera.instance.position, {x: 0.4, y: 0, z: -1.5}, 0)
     .to(camera.controls.target, {x: 0.5, y: 0, z: -2}, 0)
-    .to(bgContainer, {opacity: `0`}, 0)
+    .to(scroll, {opacity: 0}, 0)
     // .to(transition, {yPercent: 100, duration: 1, ease: "power4.out", delay: .2})
     // .to(transition, {yPercent: 200, duration: 1, ease: "power4.out"})
 
