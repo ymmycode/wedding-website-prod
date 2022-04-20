c9641be7c390aab3c35ddc5f96eb5ec81eb251ee
Achmat Fauzi <achmatfauzi86@gmail.com>
2022-04-21 06:00:27 +0700

    changing transition element target

--- src/js/Animation/locationEnterTransition.js
+++ src/js/Animation/locationEnterTransition.js
@@ -4,7 +4,10 @@ const locationEnterTransition = (camera, mapScene, container) =>
 {
     const title = container.querySelectorAll(`.title-location span`)
     const geoLink = container.querySelector(`.geo-link`)
-    const transition = document.querySelector(`.transition`)
+    const transition = container.querySelector(`.transition`)
+    const nextButton = container.querySelector(`.next-btn`)
+    const nextSpan = container.querySelectorAll(`.next-btn span`)
+    const loadingScreen = container.querySelector(`.loading-screen`)
 
     const tl = gsap.timeline({
         defaults: {
@@ -16,13 +19,17 @@ const locationEnterTransition = (camera, mapScene, container) =>
     tl
     .fromTo(transition, {yPercent: 0}, {yPercent: 100, duration: 1, delay: .2}, 0)
     .to(transition, {yPercent: 200, duration: 1}, 1.5)
+    .set(loadingScreen, {zIndex: -99}, 1.5)
     .from(title, {yPercent: 100, stagger: .1}, 2)
     .fromTo(title, {opacity: 0}, {opacity: 0.8, stagger: .1}, 2)
-    .to(camera.instance.position, {y: -55, x: 0, z: 10, duration: 3}, 1)
-    .to(camera.controls.target, {y: -60, x: 0, z: 0, duration: 3}, 1)
+    .fromTo(camera.instance.position, {y: -45, x: 0, z: 10}, {y: -55, x: 0, z: 10, duration: 3}, 1.2)
+    .fromTo(camera.controls.target, {y: -50, x: 0, z: 10}, {y: -60, x: 0, z: 0, duration: 1}, 1.2)
     .from(geoLink, {xPercent: 200, x: 0, z: 0}, 3)
     .fromTo(geoLink, {opacity: 0}, {opacity: 1}, 3)
+    .to(nextButton, {opacity: 1}, 3)
+    .from(nextSpan, {yPercent: -20, stagger: .1}, 3)
     .then(() => geoLink.classList.toggle(`animate`))
+    .then(() => camera.controls.enabled = true)
 
     return tl
 }
--- src/js/Animation/partnerEnterTransition.js
+++ src/js/Animation/partnerEnterTransition.js
@@ -3,7 +3,17 @@ import gsap from "gsap"
 const partnerEnterTransition = (camera, partner, container) => 
 {
     const title = container.querySelectorAll(`.title-partner span`)
-    const transition = document.querySelector(`.transition`)
+    const transition = container.querySelector(`.transition`)
+    const nextButton = container.querySelector(`.next-btn`)
+    const nextSpan = container.querySelectorAll(`.next-btn span`)
+
+    const part1 = document.querySelector(`.partner1`) ?? container.querySelector(`.partner1`) 
+    const part2 = document.querySelector(`.partner2`) ?? container.querySelector(`.partner2`) 
+
+    const partName = document.querySelectorAll(`.partner-name`) ?? container.querySelectorAll(`.partner-name`) 
+    const partDetail = document.querySelectorAll(`.partner-detail`) ?? container.querySelectorAll(`.partner-detail`)
+
+    const loadingScreen = container.querySelector(`.loading-screen`)
 
     camera.controls.enabled = false
 
@@ -15,19 +25,22 @@ const partnerEnterTransition = (camera, partner, container) =>
     })
 
     tl
-    .set(`.partner1`, {autoAlpha: 1})
-    .set(`.partner2`, {autoAlpha: 1})
+    .set(part1, {opacity: 1})
+    .set(part2, {opacity: 1})
     .fromTo(transition, {yPercent: 0}, {yPercent: 100, duration: 1, delay: .2}, 0)
     .to(transition, {yPercent: 200, duration: 1}, 1.5)
+    .set(loadingScreen, {zIndex: -99}, 1.5)
     .from(title, {yPercent: 100, duration: 3, stagger: .1}, 2)
     .fromTo(title, {opacity: 0}, {opacity: 0.8, duration: 3, stagger: .1}, 2)
-    .from(`.partner-name`, {yPercent: 100, stagger: .1}, 3)
-    .fromTo(`.partner-name`, {opacity: 0}, {opacity: 1,  stagger: .1}, 3)
-    .from(`.partner-detail`, {yPercent: 100, stagger: .1}, 3)
-    .fromTo(`.partner-detail`, {opacity: 0}, {opacity: 1, stagger: .1}, 3)
-    .to(camera.instance.position, {y: -39, x: 0, z: 10, duration: 3}, 1)
-    .to(camera.controls.target, {y: -40, x: 0, z: 0, duration: 3}, 1)
-
+    .from(partName, {yPercent: 100, stagger: .1}, 3)
+    .fromTo(partName, {opacity: 0}, {opacity: 1,  stagger: .1}, 3)
+    .from(partDetail, {yPercent: 100, stagger: .1}, 3)
+    .fromTo(partDetail, {opacity: 0}, {opacity: 1, stagger: .1}, 3)
+    .fromTo(camera.instance.position, {y: -30, x: 0, z: 10}, {y: -39, x: 0, z: 10, duration: 3}, 1)
+    .fromTo(camera.controls.target, {y: -30, x: 0, z: 10}, {y: -40, x: 0, z: 0, duration: 3}, 1)
+    .to(nextButton, {opacity: 1}, 3)
+    .from(nextSpan, {yPercent: -20, stagger: .1}, 3)
+    
     return tl
 }
 
