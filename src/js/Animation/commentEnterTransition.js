e89b734391d6b69c7a523021ea1ebd1cbd2d6e8e
Achmat Fauzi <achmatfauzi86@gmail.com>
2022-04-25 23:31:44 +0700

    updating transition z-index

--- src/js/Animation/commentEnterTransition.js
+++ src/js/Animation/commentEnterTransition.js
@@ -20,6 +20,7 @@ const commentEnterTransition = (camera, container) =>
     .fromTo(transition, {yPercent: 0}, {yPercent: 100, duration: 1, delay: .2}, 0)
     .to(transition, {yPercent: 200, duration: 1}, 1.5)
     .set(loadingScreen, {zIndex: -99}, 1.5)
+    .set(transition, {zIndex: -99}, 2.5)
     .set(camera.instance.position, {y: -70, x: 0, z: 10}, 1.2)
     .set(camera.controls.target, {y: -70, x: 0, z: 0}, 1.2)
     .to(commentContainer, {opacity: 1}, 2)
